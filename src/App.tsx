ddb5c088bd620fcbb03bcfed9d3819ddfd6b9d20
Dru Serkes <dru@zephyrx.com>
2021-07-07 18:08:26 -0400

    add TODOs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import './App.css';
 
+/*
+TODO 
+0. Add useLocalStorage custom hook 
+1. Add a controlled - text field where a user can type 
+2. Test it 
+3. Add character / word count component 
+4. Test it 
+5. Add a component providing letter density insights 
+6. Test it 
+7. Add copy / clear buttons 
+8. Test them 
+9. Add social media insights component 
+10. Test them 
+11. Decide what other features to add 
+*/
+
 export const App: React.FC = () => {
   return (
     <div className="App">
