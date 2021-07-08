85d20226f87bedd0320f8db1f664f3a7fabe4162
Dru Serkes <dru@zephyrx.com>
2021-07-08 18:04:42 -0400

    add UserInput component

---
+++ src/components/UserInput.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+interface UserInputProps {
+  input?: string,
+  setInput: React.Dispatch<React.SetStateAction<string>>
+}
+
+export const UserInput: React.FC<UserInputProps> = ({ input, setInput }) => {
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => setInput(e.target.value);
+  return (
+    <main className="UserInput">
+      <label htmlFor="bio">Bio</label>
+      <textarea id="bio" name="bio" placeholder="Start typing your bio here..." value={input} onChange={handleChange} />
+    </main>
+  )
+};
